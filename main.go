6460d2baea002df03daa4cd3fed7fe5c00a0c58b
Aliaksey Artamonau <aliaksiej.artamonau@gmail.com>
2013-06-26 12:57:57 -0700

    Split glpk-independent code into separate file.

---
+++ main.go
@@ -0,0 +1,172 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"math/rand"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+	"log"
+)
+
+type TagHist []uint
+
+var (
+	seed         int64
+	tagHistogram TagHist     = nil
+	params       VbmapParams = VbmapParams{
+		Tags: nil,
+	}
+)
+
+func (tags *TagMap) Set(s string) error {
+	*tags = make(TagMap)
+
+	for _, pair := range strings.Split(s, ",") {
+		tagNode := strings.Split(pair, ":")
+		if len(tagNode) != 2 {
+			return fmt.Errorf("Invalid tag-node pair: %s", pair)
+		}
+
+		node, err := strconv.ParseUint(tagNode[0], 10, strconv.IntSize)
+		if err != nil {
+			return err
+		}
+
+		tag, err := strconv.ParseUint(tagNode[1], 10, strconv.IntSize)
+		if err != nil {
+			return err
+		}
+
+		(*tags)[Node(node)] = Tag(tag)
+	}
+	return nil
+}
+
+func (hist *TagHist) Set(s string) error {
+	values := strings.Split(s, ",")
+	*hist = make(TagHist, len(values))
+
+	for i, v := range values {
+		count, err := strconv.ParseUint(v, 10, strconv.IntSize)
+		if err != nil {
+			return err
+		}
+
+		(*hist)[i] = uint(count)
+	}
+
+	return nil
+}
+
+func (hist TagHist) String() string {
+	return fmt.Sprintf("%v", []uint(hist))
+}
+
+func checkInput() {
+	if params.NumNodes <= 0 || params.NumSlaves <= 0 || params.NumVBuckets <= 0 {
+		log.Fatalf("num-nodes, num-slaves and num-vbuckets must be greater than zero")
+	}
+
+	if params.NumReplicas < 0 {
+		log.Fatalf("num-replicas must be greater of equal than zero")
+	}
+
+	if params.NumReplicas+1 > params.NumNodes {
+		params.NumReplicas = params.NumNodes - 1
+	}
+
+	if params.NumSlaves >= params.NumNodes {
+		params.NumSlaves = params.NumNodes - 1
+	}
+
+	if params.NumSlaves < params.NumReplicas {
+		params.NumReplicas = params.NumSlaves
+	}
+
+	if params.Tags != nil && tagHistogram != nil {
+		log.Fatalf("Options --tags and --tag-histogram are exclusive")
+	}
+
+	if params.Tags == nil && tagHistogram == nil {
+		log.Printf("Tags are not specified. Assuming every node on a separate tag.")
+		tagHistogram = make(TagHist, params.NumNodes)
+
+		for i := 0; i < params.NumNodes; i++ {
+			tagHistogram[i] = 1
+		}
+	}
+
+	if tagHistogram != nil {
+		tag := 0
+		params.Tags = make(TagMap)
+
+		for i := 0; i < params.NumNodes; i++ {
+			for tag < len(tagHistogram) && tagHistogram[tag] == 0 {
+				tag += 1
+			}
+			if tag >= len(tagHistogram) {
+				log.Fatalf("Invalid tag histogram. Counts do not add up.")
+			}
+
+			tagHistogram[tag] -= 1
+			params.Tags[Node(i)] = Tag(tag)
+		}
+
+		if tag != len(tagHistogram)-1 || tagHistogram[tag] != 0 {
+			log.Fatalf("Invalid tag histogram. Counts do not add up.")
+		}
+	}
+
+	// each node should have a tag assigned
+	for i := 0; i < params.NumNodes; i++ {
+		_, present := params.Tags[Node(i)]
+		if !present {
+			log.Fatalf("Tag for node %v not specified", i)
+		}
+	}
+}
+
+func main() {
+	log.SetOutput(os.Stderr)
+	log.SetFlags(0)
+
+	// TODO
+	flag.IntVar(&params.NumNodes, "num-nodes", 25, "Number of nodes")
+	flag.IntVar(&params.NumSlaves, "num-slaves", 10, "Number of slaves")
+	flag.IntVar(&params.NumVBuckets, "num-vbuckets", 1024, "Number of VBuckets")
+	flag.IntVar(&params.NumReplicas, "num-replicas", 1, "Number of replicas")
+	flag.Var(&params.Tags, "tags", "Tags")
+	flag.Var(&tagHistogram, "tag-histogram", "Tag histogram")
+
+	flag.Int64Var(&seed, "seed", time.Now().UTC().UnixNano(), "Random seed")
+
+	flag.Parse()
+
+	rand.Seed(seed)
+
+	checkInput()
+
+	log.Printf("Finalized parameters")
+	log.Printf("  Number of nodes: %d", params.NumNodes)
+	log.Printf("  Number of slaves: %d", params.NumSlaves)
+	log.Printf("  Number of vbuckets: %d", params.NumVBuckets)
+	log.Printf("  Number of replicas: %d", params.NumReplicas)
+	log.Printf("  Tags assignments:")
+
+	for i := 0; i < params.NumNodes; i++ {
+		log.Printf("    %d -> %v", i, params.Tags[Node(i)])
+	}
+
+	solution, err := VbmapGenerate(params)
+	if err != nil {
+		log.Fatalf("Failed to find a solution (%s)", err.Error())
+	}
+
+	log.Printf("Solution I got:\n")
+	for v, chain := range solution {
+		log.Printf("%4d : %v", v, chain)
+	}
+}
--- vbmap.go
+++ vbmap.go
@@ -1,20 +1,16 @@
 package main
 
 import (
-	"flag"
 	"fmt"
-	"math/rand"
-	"os"
-	"strconv"
-	"strings"
-	"time"
+	"bytes"
 	"log"
+	"math/rand"
+	"container/heap"
 )
 
 type Node int
 type Tag uint
 type TagMap map[Node]Tag
-type TagHist []uint
 
 type VbmapParams struct {
 	Tags TagMap
@@ -23,38 +19,6 @@ type VbmapParams struct {
 	NumSlaves   int
 	NumVBuckets int
 	NumReplicas int
-}
-
-var (
-	seed         int64
-	tagHistogram TagHist     = nil
-	params       VbmapParams = VbmapParams{
-		Tags: nil,
-	}
-)
-
-func (tags *TagMap) Set(s string) error {
-	*tags = make(TagMap)
-
-	for _, pair := range strings.Split(s, ",") {
-		tagNode := strings.Split(pair, ":")
-		if len(tagNode) != 2 {
-			return fmt.Errorf("Invalid tag-node pair: %s", pair)
-		}
-
-		node, err := strconv.ParseUint(tagNode[0], 10, strconv.IntSize)
-		if err != nil {
-			return err
-		}
-
-		tag, err := strconv.ParseUint(tagNode[1], 10, strconv.IntSize)
-		if err != nil {
-			return err
-		}
-
-		(*tags)[Node(node)] = Tag(tag)
-	}
-	return nil
 }
 
 func (tags TagMap) String() string {
@@ -71,128 +35,564 @@ func (tags TagMap) TagsCount() int {
 	return len(seen)
 }
 
-func (hist *TagHist) Set(s string) error {
-	values := strings.Split(s, ",")
-	*hist = make(TagHist, len(values))
+type RCandidate struct {
+	params VbmapParams
+	matrix [][]int
 
-	for i, v := range values {
-		count, err := strconv.ParseUint(v, 10, strconv.IntSize)
-		if err != nil {
-			return err
-		}
+	rowSums          []int
+	colSums          []int
+	expectedColSum   int
+	expectedOutliers int
 
-		(*hist)[i] = uint(count)
-	}
-
-	return nil
+	outliers      int
+	rawEvaluation int
 }
 
-func (hist TagHist) String() string {
-	return fmt.Sprintf("%v", []uint(hist))
-}
+func buildInitialR(params VbmapParams, RI [][]int) (R [][]int) {
+	activeVbsPerNode := SpreadSum(params.NumVBuckets, params.NumNodes)
 
-func checkInput() {
-	if params.NumNodes <= 0 || params.NumSlaves <= 0 || params.NumVBuckets <= 0 {
-		log.Fatalf("num-nodes, num-slaves and num-vbuckets must be greater than zero")
+	R = make([][]int, len(RI))
+	if params.NumSlaves == 0 {
+		return
 	}
 
-	if params.NumReplicas < 0 {
-		log.Fatalf("num-replicas must be greater of equal than zero")
-	}
+	for i, row := range RI {
+		rowSum := activeVbsPerNode[i] * params.NumReplicas
+		slaveVbs := SpreadSum(rowSum, params.NumSlaves)
 
-	if params.NumReplicas+1 > params.NumNodes {
-		params.NumReplicas = params.NumNodes - 1
-	}
+		R[i] = make([]int, len(row))
 
-	if params.NumSlaves >= params.NumNodes {
-		params.NumSlaves = params.NumNodes - 1
-	}
-
-	if params.NumSlaves < params.NumReplicas {
-		params.NumReplicas = params.NumSlaves
-	}
-
-	if params.Tags != nil && tagHistogram != nil {
-		log.Fatalf("Options --tags and --tag-histogram are exclusive")
-	}
-
-	if params.Tags == nil && tagHistogram == nil {
-		log.Printf("Tags are not specified. Assuming every node on a separate tag.")
-		tagHistogram = make(TagHist, params.NumNodes)
-
-		for i := 0; i < params.NumNodes; i++ {
-			tagHistogram[i] = 1
-		}
-	}
-
-	if tagHistogram != nil {
-		tag := 0
-		params.Tags = make(TagMap)
-
-		for i := 0; i < params.NumNodes; i++ {
-			for tag < len(tagHistogram) && tagHistogram[tag] == 0 {
-				tag += 1
+		slave := 0
+		for j, elem := range row {
+			if elem != 0 {
+				R[i][j] = slaveVbs[slave]
+				slave += 1
 			}
-			if tag >= len(tagHistogram) {
-				log.Fatalf("Invalid tag histogram. Counts do not add up.")
-			}
-
-			tagHistogram[tag] -= 1
-			params.Tags[Node(i)] = Tag(tag)
-		}
-
-		if tag != len(tagHistogram)-1 || tagHistogram[tag] != 0 {
-			log.Fatalf("Invalid tag histogram. Counts do not add up.")
 		}
 	}
 
-	// each node should have a tag assigned
-	for i := 0; i < params.NumNodes; i++ {
-		_, present := params.Tags[Node(i)]
-		if !present {
-			log.Fatalf("Tag for node %v not specified", i)
+	return
+}
+
+func makeRCandidate(params VbmapParams, RI [][]int) (result RCandidate) {
+	result.params = params
+	result.matrix = buildInitialR(params, RI)
+	result.colSums = make([]int, params.NumNodes)
+	result.rowSums = make([]int, params.NumNodes)
+
+	for i, row := range result.matrix {
+		rowSum := 0
+		for j, elem := range row {
+			rowSum += elem
+			result.colSums[j] += elem
+		}
+		result.rowSums[i] = rowSum
+	}
+
+	numReplications := params.NumVBuckets * params.NumReplicas
+	result.expectedColSum = numReplications / params.NumNodes
+	result.expectedOutliers = numReplications % params.NumNodes
+
+	for _, sum := range result.colSums {
+		result.rawEvaluation += Abs(sum - result.expectedColSum)
+		if sum == result.expectedColSum+1 {
+			result.outliers += 1
+		}
+	}
+
+	return
+}
+
+func (cand RCandidate) computeEvaluation(rawEval int, outliers int) (eval int) {
+	eval = rawEval
+	if outliers > cand.expectedOutliers {
+		eval -= cand.expectedOutliers
+	} else {
+		eval -= outliers
+	}
+
+	return
+}
+
+func (cand RCandidate) evaluation() int {
+	return cand.computeEvaluation(cand.rawEvaluation, cand.outliers)
+}
+
+func (cand RCandidate) swapOutliersChange(row int, j int, k int) (change int) {
+	a, b := cand.matrix[row][j], cand.matrix[row][k]
+	ca := cand.colSums[j] - a + b
+	cb := cand.colSums[k] - b + a
+
+	if cand.colSums[j] == cand.expectedColSum+1 {
+		change -= 1
+	}
+	if cand.colSums[k] == cand.expectedColSum+1 {
+		change -= 1
+	}
+	if ca == cand.expectedColSum+1 {
+		change += 1
+	}
+	if cb == cand.expectedColSum+1 {
+		change += 1
+	}
+
+	return
+}
+
+func (cand RCandidate) swapRawEvaluationChange(row int, j int, k int) (change int) {
+	a, b := cand.matrix[row][j], cand.matrix[row][k]
+	ca := cand.colSums[j] - a + b
+	cb := cand.colSums[k] - b + a
+
+	evalA := Abs(ca - cand.expectedColSum)
+	evalB := Abs(cb - cand.expectedColSum)
+
+	oldEvalA := Abs(cand.colSums[j] - cand.expectedColSum)
+	oldEvalB := Abs(cand.colSums[k] - cand.expectedColSum)
+
+	change = evalA - oldEvalA + evalB - oldEvalB
+
+	return
+}
+
+func (cand RCandidate) swapBenefit(row int, j int, k int) int {
+	eval := cand.evaluation()
+
+	swapOutliers := cand.outliers + cand.swapOutliersChange(row, j, k)
+	swapRawEval := cand.rawEvaluation + cand.swapRawEvaluationChange(row, j, k)
+	swapEval := cand.computeEvaluation(swapRawEval, swapOutliers)
+
+	return swapEval - eval
+}
+
+func (cand *RCandidate) swapElems(row int, j int, k int) {
+	if cand.matrix[row][j] == 0 || cand.matrix[row][k] == 0 {
+		panic(fmt.Sprintf("swapping one or more zeros (%d: %d <-> %d)",
+			row, j, k))
+	}
+
+	cand.rawEvaluation += cand.swapRawEvaluationChange(row, j, k)
+	cand.outliers += cand.swapOutliersChange(row, j, k)
+
+	a, b := cand.matrix[row][j], cand.matrix[row][k]
+
+	cand.colSums[j] += b - a
+	cand.colSums[k] += a - b
+	cand.matrix[row][j], cand.matrix[row][k] = b, a
+}
+
+func (cand RCandidate) dump() {
+	buffer := &bytes.Buffer{}
+
+	fmt.Fprintf(buffer, "    |")
+	for i := 0; i < cand.params.NumNodes; i++ {
+		fmt.Fprintf(buffer, "%3d ", cand.params.Tags[Node(i)])
+	}
+	fmt.Fprintf(buffer, "|\n")
+
+	fmt.Fprintf(buffer, "----|")
+	for i := 0; i < cand.params.NumNodes; i++ {
+		fmt.Fprintf(buffer, "----")
+	}
+	fmt.Fprintf(buffer, "|\n")
+
+	for i, row := range cand.matrix {
+		fmt.Fprintf(buffer, "%3d |", cand.params.Tags[Node(i)])
+		for _, elem := range row {
+			fmt.Fprintf(buffer, "%3d ", elem)
+		}
+		fmt.Fprintf(buffer, "| %d\n", cand.rowSums[i])
+	}
+
+	fmt.Fprintf(buffer, "____|")
+	for i := 0; i < cand.params.NumNodes; i++ {
+		fmt.Fprintf(buffer, "____")
+	}
+	fmt.Fprintf(buffer, "|\n")
+
+	fmt.Fprintf(buffer, "    |")
+	for i := 0; i < cand.params.NumNodes; i++ {
+		fmt.Fprintf(buffer, "%3d ", cand.colSums[i])
+	}
+	fmt.Fprintf(buffer, "|\n")
+	fmt.Fprintf(buffer, "Evaluation: %d\n", cand.evaluation())
+
+	log.Print(buffer)
+}
+
+func (cand RCandidate) copy() (result RCandidate) {
+	result.params = cand.params
+	result.expectedColSum = cand.expectedColSum
+	result.expectedOutliers = cand.expectedOutliers
+	result.outliers = cand.outliers
+	result.rawEvaluation = cand.rawEvaluation
+
+	result.matrix = make([][]int, cand.params.NumNodes)
+	for i, row := range cand.matrix {
+		result.matrix[i] = make([]int, cand.params.NumNodes)
+		copy(result.matrix[i], row)
+	}
+
+	result.rowSums = make([]int, cand.params.NumNodes)
+	copy(result.rowSums, cand.rowSums)
+
+	result.colSums = make([]int, cand.params.NumNodes)
+	copy(result.colSums, cand.colSums)
+
+	return
+}
+
+type TabuPair struct {
+	row, j, k int
+}
+
+type TabuElem struct {
+	row, col int
+}
+
+type Tabu struct {
+	tabu        map[TabuPair]int
+	elemIndex   map[TabuElem]TabuPair
+	expireIndex map[int]TabuPair
+}
+
+func makeTabuPair(row int, j int, k int) TabuPair {
+	if j > k {
+		j, k = k, j
+	}
+	return TabuPair{row, j, k}
+}
+
+func makeTabu() Tabu {
+	return Tabu{make(map[TabuPair]int),
+		make(map[TabuElem]TabuPair),
+		make(map[int]TabuPair)}
+}
+
+func (tabu Tabu) add(time int, row int, j int, k int) {
+	oldItem, present := tabu.elemIndex[TabuElem{row, j}]
+	if present {
+		tabu.expire(tabu.tabu[oldItem])
+	}
+
+	oldItem, present = tabu.elemIndex[TabuElem{row, k}]
+	if present {
+		tabu.expire(tabu.tabu[oldItem])
+	}
+
+	item := makeTabuPair(row, j, k)
+	tabu.tabu[item] = time
+	tabu.expireIndex[time] = item
+
+	tabu.elemIndex[TabuElem{row, j}] = item
+	tabu.elemIndex[TabuElem{row, k}] = item
+}
+
+func (tabu Tabu) member(row int, j int, k int) bool {
+	_, present := tabu.tabu[makeTabuPair(row, j, k)]
+	return present
+}
+
+func (tabu Tabu) expire(time int) {
+	item := tabu.expireIndex[time]
+	delete(tabu.expireIndex, time)
+	delete(tabu.tabu, item)
+	delete(tabu.elemIndex, TabuElem{item.row, item.j})
+	delete(tabu.elemIndex, TabuElem{item.row, item.k})
+}
+
+func doBuildR(params VbmapParams, RI [][]int) (best RCandidate) {
+	cand := makeRCandidate(params, RI)
+	best = cand.copy()
+
+	if params.NumSlaves <= 1 || params.NumReplicas == 0 {
+		// nothing to optimize here; just return
+		return
+	}
+
+	attempts := 10 * params.NumNodes * params.NumNodes
+	expire := 10 * params.NumNodes
+	noImprovementLimit := params.NumNodes * params.NumNodes
+
+	highElems := make([]int, params.NumNodes)
+	lowElems := make([]int, params.NumNodes)
+
+	candidateRows := make([]int, params.NumNodes)
+	tabu := makeTabu()
+
+	noCandidate := 0
+	swapTabued := 0
+	swapDecreased := 0
+	swapIndifferent := 0
+	swapIncreased := 0
+
+	t := 0
+	noImprovementIters := 0
+
+	for t = 0; t < attempts; t++ {
+		if t >= expire {
+			tabu.expire(t - expire)
+		}
+
+		if noImprovementIters > noImprovementLimit {
+			break
+		}
+
+		noImprovementIters++
+
+		if best.evaluation() == 0 {
+			break
+		}
+
+		highElems = []int{}
+		lowElems = []int{}
+
+		candidateRows = []int{}
+
+		for i, elem := range cand.colSums {
+			switch {
+			case elem <= cand.expectedColSum:
+				lowElems = append(lowElems, i)
+			case elem > cand.expectedColSum:
+				highElems = append(highElems, i)
+			}
+		}
+
+		lowIx := lowElems[rand.Intn(len(lowElems))]
+		highIx := highElems[rand.Intn(len(highElems))]
+
+		for row := 0; row < params.NumNodes; row++ {
+			lowElem := cand.matrix[row][lowIx]
+			highElem := cand.matrix[row][highIx]
+
+			if lowElem != 0 && highElem != 0 && highElem != lowElem {
+				benefit := cand.swapBenefit(row, lowIx, highIx)
+
+				if benefit >= 0 && rand.Intn(20) != 0 {
+					continue
+				}
+
+				candidateRows = append(candidateRows, row)
+			}
+		}
+
+		if len(candidateRows) == 0 {
+			noCandidate++
+			continue
+		}
+
+		row := candidateRows[rand.Intn(len(candidateRows))]
+
+		if tabu.member(row, lowIx, highIx) {
+			swapTabued++
+			continue
+		}
+
+		old := cand.evaluation()
+
+		cand.swapElems(row, lowIx, highIx)
+		tabu.add(t, row, lowIx, highIx)
+
+		if old == cand.evaluation() {
+			swapIndifferent++
+		} else if old < cand.evaluation() {
+			swapIncreased++
+		} else {
+			swapDecreased++
+		}
+
+		if cand.evaluation() < best.evaluation() {
+			best = cand.copy()
+			noImprovementIters = 0
+		}
+	}
+
+	log.Printf("Search stats")
+	log.Printf("  iters -> %d", t)
+	log.Printf("  no improvement termination? -> %v",
+		noImprovementIters == noImprovementLimit)
+	log.Printf("  noCandidate -> %d", noCandidate)
+	log.Printf("  swapTabued -> %d", swapTabued)
+	log.Printf("  swapDecreased -> %d", swapDecreased)
+	log.Printf("  swapIndifferent -> %d", swapIndifferent)
+	log.Printf("  swapIncreased -> %d", swapIncreased)
+	log.Printf("")
+
+	return
+}
+
+func buildR(params VbmapParams, RI [][]int) (best RCandidate) {
+	bestEvaluation := (1 << 31) - 1
+
+	for i := 0; i < 10; i++ {
+		R := doBuildR(params, RI)
+		if R.evaluation() < bestEvaluation {
+			best = R
+			bestEvaluation = R.evaluation()
+		}
+
+		if bestEvaluation == 0 {
+			log.Printf("Found balanced map R after %d attempts", i)
+			break
+		}
+	}
+
+	return
+}
+
+type Vbmap [][]Node
+
+func makeVbmap (params VbmapParams) (vbmap Vbmap) {
+	vbmap = make([][]Node, params.NumVBuckets)
+	for v := 0; v < params.NumVBuckets; v++ {
+		vbmap[v] = make([]Node, params.NumReplicas + 1)
+	}
+
+	return
+}
+
+type Slave struct {
+	index int
+	count int
+	stats []int
+}
+
+type SlaveHeap []Slave
+
+func makeSlave(index int, count int, params VbmapParams) (slave Slave) {
+	slave.index = index
+	slave.count = count
+	slave.stats = make([]int, params.NumReplicas)
+
+	return
+}
+
+func (h SlaveHeap) Len() int {
+	return len(h)
+}
+
+func (h SlaveHeap) Less(i, j int) bool {
+	switch {
+	case h[i].count > h[j].count:
+		return true
+	case h[i].count == h[j].count:
+		return h[i].index < h[j].index
+	default:
+		return false
+	}
+}
+
+func (h SlaveHeap) Swap(i, j int) {
+	h[i], h[j] = h[j], h[i]
+}
+
+func (h *SlaveHeap) Push(x interface{}) {
+	*h = append(*h, x.(Slave))
+}
+
+func (h *SlaveHeap) Pop() interface{} {
+	old := *h
+	n := len(old)
+	x := old[n - 1]
+	*h = old[0 : n - 1]
+	return x
+}
+
+func reorderReplicas(replicas []Slave) {
+	for turn := range replicas {
+		for i := turn + 1; i < len(replicas); i++ {
+			if replicas[i].stats[turn] < replicas[turn].stats[turn] {
+				replicas[i], replicas[turn] =
+					replicas[turn], replicas[i]
+			}
 		}
 	}
 }
 
-func main() {
-	log.SetOutput(os.Stderr)
-	log.SetFlags(0)
+func buildVbmap(R RCandidate) (vbmap Vbmap) {
+	params := R.params
+	vbmap = makeVbmap(params)
 
-	// TODO
-	flag.IntVar(&params.NumNodes, "num-nodes", 25, "Number of nodes")
-	flag.IntVar(&params.NumSlaves, "num-slaves", 10, "Number of slaves")
-	flag.IntVar(&params.NumVBuckets, "num-vbuckets", 1024, "Number of VBuckets")
-	flag.IntVar(&params.NumReplicas, "num-replicas", 1, "Number of replicas")
-	flag.Var(&params.Tags, "tags", "Tags")
-	flag.Var(&tagHistogram, "tag-histogram", "Tag histogram")
+	var nodeVbs []int
+	if params.NumReplicas == 0 || params.NumSlaves == 0 {
+		nodeVbs = SpreadSum(params.NumVBuckets, params.NumNodes)
+	} else {
+		nodeVbs = make([]int, params.NumNodes)
+		for i, sum := range R.rowSums {
+			vbs := sum / params.NumReplicas
+			if sum % params.NumReplicas != 0 {
+				panic("row sum is not multiple of NumReplicas")
+			}
 
-	flag.Int64Var(&seed, "seed", time.Now().UTC().UnixNano(), "Random seed")
-
-	flag.Parse()
-
-	rand.Seed(seed)
-
-	checkInput()
-
-	log.Printf("Finalized parameters")
-	log.Printf("  Number of nodes: %d", params.NumNodes)
-	log.Printf("  Number of slaves: %d", params.NumSlaves)
-	log.Printf("  Number of vbuckets: %d", params.NumVBuckets)
-	log.Printf("  Number of replicas: %d", params.NumReplicas)
-	log.Printf("  Tags assignments:")
-
-	for i := 0; i < params.NumNodes; i++ {
-		log.Printf("    %d -> %v", i, params.Tags[Node(i)])
+			nodeVbs[i] = vbs
+		}
 	}
 
-	solution, err := VbmapGenerate(params)
+	vbucket := 0
+	for i, row := range R.matrix {
+		slaves := &SlaveHeap{}
+		heap.Init(slaves)
+
+		vbs := nodeVbs[i]
+
+		for s, count := range row {
+			if count != 0 {
+				heap.Push(slaves, makeSlave(s, count, params))
+			}
+		}
+
+		if slaves.Len() == 0 {
+			for vbs > 0 {
+				vbmap[vbucket][0] = Node(i)
+				vbs--
+				vbucket++
+			}
+
+			continue
+		}
+
+		for vbs > 0 {
+			replicas := make([]Slave, params.NumReplicas)
+			vbmap[vbucket][0] = Node(i)
+
+			for r := 0; r < params.NumReplicas; r++ {
+				if slaves.Len() == 0 {
+					panic("Ran out of slaves")
+				}
+
+				slave := heap.Pop(slaves).(Slave)
+				replicas[r] = slave
+			}
+
+			reorderReplicas(replicas)
+
+			for turn, slave := range replicas {
+				slave.count--
+				slave.stats[turn]++
+
+				vbmap[vbucket][turn + 1] = Node(slave.index)
+
+				if slave.count != 0 {
+					heap.Push(slaves, slave)
+				}
+			}
+
+			vbs--
+			vbucket++
+		}
+	}
+
+	return
+}
+
+func VbmapGenerate(params VbmapParams) (vbmap Vbmap, err error) {
+	RI, err := invokeGlpk(params)
 	if err != nil {
-		log.Fatalf("Failed to find a solution (%s)", err.Error())
+		return
 	}
 
-	log.Printf("Solution I got:\n")
-	for v, chain := range solution.vbmap {
-		log.Printf("%4d : %v", v, chain)
-	}
+	R := buildR(params, RI)
+	R.dump()
+
+	return buildVbmap(R), nil
 }
