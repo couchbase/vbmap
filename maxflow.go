9ad6f9a0da1b7f66e0926256bff4125cd9887b4d
Aliaksey Artamonau <aliaksiej.artamonau@gmail.com>
2013-10-22 20:26:56 -0700

    Fix augPath methods that mutate the path.

--- maxflow.go
+++ maxflow.go
@@ -179,18 +179,18 @@ func (edge graphEdge) isSaturated() bool {
 
 type augPath []*graphEdge
 
-func (path augPath) addEdge(edge *graphEdge) {
-	path = append(path, edge)
+func (path *augPath) addEdge(edge *graphEdge) {
+	*path = append(*path, edge)
 }
 
-func (path augPath) removeLastEdge() (edge *graphEdge) {
-	n := len(path)
+func (path *augPath) removeLastEdge() (edge *graphEdge) {
+	n := len(*path)
 	if n == 0 {
 		panic("Removing edge from empty path")
 	}
 
-	edge = path[n-1]
-	path = path[0 : n-1]
+	edge = (*path)[n-1]
+	*path = (*path)[0 : n-1]
 
 	return
 }
